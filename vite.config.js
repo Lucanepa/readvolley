15f747706b5cfc9ce20fef9161d345d98ae3bc30
Luca Canepa <luca.canepa@gmail.com>
2026-01-15 16:25:23 +0100

    feat: Update favicon to `favicon.ico`, streamline PWA asset inclusion, and add maskable purpose to manifest icons.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         tailwindcss(),
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            includeAssets: ['favicon.ico'],
             manifest: {
                 name: 'Volleyball Rules PWA',
                 short_name: 'ReadVolley',
@@ -20,12 +20,14 @@ export default defineConfig({
                     {
                         src: 'pwa-192x192.png',
                         sizes: '192x192',
-                        type: 'image/png'
+                        type: 'image/png',
+                        purpose: 'any maskable'
                     },
                     {
                         src: 'pwa-512x512.png',
                         sizes: '512x512',
-                        type: 'image/png'
+                        type: 'image/png',
+                        purpose: 'any maskable'
                     }
                 ]
             }
